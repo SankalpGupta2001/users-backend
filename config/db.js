c987ac9927555637cc95c7d9fb7704537c308531
SankalpGupta2001 <83775048+SankalpGupta2001@users.noreply.github.com>
2023-12-17 12:06:17 -0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
      
-      await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+      await mongoose.connect("mongodb+srv://saralgupta2001:sFbGLgVFxn15r1Zo@cluster0.pxynaxi.mongodb.net/users?retryWrites=true&w=majority"
+, { useNewUrlParser: true, useUnifiedTopology: true });
       console.log('Connected to MongoDB successfully!');
     } catch (error) {
       console.error('Error connecting to MongoDB:', error);
